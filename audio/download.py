f9af38e8182d00ffa246418940f57fab387dbb19
Brady Moon <bradygmoon@gmail.com>
2024-01-25 12:01:12 -0500

    Fix the All and Date_Range option in the audio download

--- audio/download.py
+++ audio/download.py
@@ -59,6 +59,7 @@ args = parser.parse_args()
 
 # Print the selected option
 print(f'Selected Option: {args.option}')
+print(f'Selected Location: {args.location}')
 # Print the selected values for weather type, visibility threshold and sky cover
 if args.option == 'Date_Range':
     print(f'Selected Start Date: {args.start_date}')
@@ -87,12 +88,11 @@ if args.option == 'Date_Range' or args.option == 'All':
     for location in locations:
         # Go through each date in range
         for year in range(int(start_date.split('-')[0]), int(end_date.split('-')[0])+1):
-            for month in range(int(start_date.split('-')[1]), int(end_date.split('-')[1])+1):
-                # Create the date string
-                date = f'{year}/{month:02d}'
-                # Create the path to download
+            start_month = int(start_date.split('-')[1]) if year == int(start_date.split('-')[0]) else 1
+            end_month = int(end_date.split('-')[1]) if year == int(end_date.split('-')[0]) else 12
+            for month in range(start_month, end_month+1):
                 for day in range(1, 32):
-                    audio_path = f'{location}/{date}/{month:02d}-{day:02d}-{str(year)[-2:]}_audio'
+                    audio_path = f'{location}/{year}/{month:02d}/{month:02d}-{day:02d}-{str(year)[-2:]}_audio'
                     download_file(base_url, args.save_dir, audio_path)
 elif args.option == 'Sample':
     sample_audio_path = 'kbtp/2020/11/11-02-20_audio'
@@ -113,3 +113,6 @@ elif args.option == 'Raw':
     # Exit if download fails
     elif ret != 0:
         print(f'Failed to download {audio_path}')
+else:
+    print('Invalid option')
+
